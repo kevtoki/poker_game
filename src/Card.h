310cd649d7ab57ac3f5f7c8e4c12b79b21be0b63
JONG <jongd1@crystalcove.eecs.uci.edu>
2022-05-17 14:30:51 -0700

    tried out git commands and made a Card file in Card.h

--- src/Card.h
+++ src/Card.h
@@ -1,7 +1,13 @@
 #ifndef CARD_H
 #define CARD_H
 
-
+enum Suit{spade, clubs, diamonds, hearts};
+enum Rank{ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king};
+struct Card
+{
+	Suit suit;
+	Rank rank;
+}
 
 
 #endif
